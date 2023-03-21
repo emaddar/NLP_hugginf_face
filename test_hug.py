8409c376ba10133349bc9cb8b429448a8233e7f0
Emad_Simplon <e.a.darwich@gmail.com>
2023-03-21 10:28:55 +0100

    streamlit

--- test_hug.py
+++ test_hug.py
@@ -7,7 +7,13 @@ tokenizer = AutoTokenizer.from_pretrained("dslim/bert-base-NER")
 model = AutoModelForTokenClassification.from_pretrained("dslim/bert-base-NER")
 
 nlp = pipeline("ner", model=model, tokenizer=tokenizer)
-example = "My name is Sarah and I live in Canada "
+example = st.text_area("you text")
 
 ner_results = nlp(example)
-st.write(ner_results)
+
+import pandas as pd
+
+# assuming your data is stored in a variable called 'data'
+df = pd.DataFrame(ner_results)
+
+st.write(df)
